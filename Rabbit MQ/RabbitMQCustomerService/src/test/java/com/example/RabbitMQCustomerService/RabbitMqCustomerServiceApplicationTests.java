10ea5af54ead87563a433d9976ef6ac5bfc0b018
roshan0006 <68911081+roshan0006@users.noreply.github.com>
2022-05-05 23:52:18 +0530

    Changes comitted

---
+++ Rabbit MQ/RabbitMQCustomerService/src/test/java/com/example/RabbitMQCustomerService/RabbitMqCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RabbitMQCustomerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
